e6c4a4ebf19eff93528f755a60bd280488894d8e
Gel <angel.balano@gmail.com>
2024-08-10 21:44:23 +0800

    Add before block in test

--- tests/test-app-pda.ts
+++ tests/test-app-pda.ts
@@ -1,16 +1,58 @@
 import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
 import { TestAppPda } from "../target/types/test_app_pda";
-import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
-import { assert } from "chai";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { readFileSync } from 'fs'
+import { homedir } from 'os'
+import { resolve } from 'path'
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 
+function loadKeypair(filePath: string): Keypair {
+  const resolvedPath = filePath.startsWith('~')
+    ? filePath.replace('~', homedir())
+    : filePath
+  const absolutePath = resolve(resolvedPath)
+  const keypairString = readFileSync(absolutePath, 'utf-8')
+  const keypairBuffer = Buffer.from(JSON.parse(keypairString))
+  return Keypair.fromSecretKey(keypairBuffer)
+}
 describe("test-app-pda", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.TestAppPda as Program<TestAppPda>;
-  before(async () => {
+  const authority = loadKeypair('~/.config/solana/id.json')
 
+  let tokenMint: PublicKey
+  let ownerAta: PublicKey
+
+  before(async () => {
+    tokenMint = await createMint(
+      program.provider.connection,
+      authority,
+      authority.publicKey,
+      authority.publicKey,
+      9
+    )
+
+    ownerAta = (
+      await getOrCreateAssociatedTokenAccount(
+        program.provider.connection,
+        authority,
+        tokenMint,
+        authority.publicKey
+      )
+    ).address
+
+    await Promise.all([
+      mintTo(
+        program.provider.connection,
+        authority,
+        tokenMint,
+        ownerAta,
+        authority,
+        1000 * 10 ** 9
+      )])
   })
 
   it("Is initialized!", async () => {
