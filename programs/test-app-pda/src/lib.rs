e6140ac37413d03100924fa31701a7f4a77f53c6
Gel <angel.balano@gmail.com>
2024-08-10 21:03:14 +0800

    Refactor deposit escrow

--- programs/test-app-pda/src/lib.rs
+++ programs/test-app-pda/src/lib.rs
@@ -1,55 +1,137 @@
 use anchor_lang::prelude::*;
-use std::mem::size_of;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
+use anchor_spl::associated_token::AssociatedToken;
 
-declare_id!("JAqrRSvCSUkAb77kFMpwe7VXkpPSfDDpsyMwBxApNjvd");
+declare_id!("3U1szxp8AY1YCQ98gEGwjzW9ntVUUZNSAV2oKEeZRTNr");
 
 #[program]
 pub mod test_app_pda {
     use super::*;
 
-    pub fn create_escrow(ctx: Context<CreateEscrow>, amount: u64) -> Result<()> {
-      // get escrow account
-      let escrow = &mut ctx.accounts.escrow;
+    pub fn deposit_escrow(ctx: Context<InitializeEscrow>, params: InitializeEscrowParams) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.owner = ctx.accounts.owner.key();
+        escrow.bump = ctx.bumps.escrow;
+        // Transfer tokens from owner to escrow account
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.owner_token_account.to_account_info(),
+                    to: ctx.accounts.escrow_token_account.to_account_info(),
+                    authority: ctx.accounts.owner.to_account_info(),
+                },
+            ),
+            params.amount,
+        )?;
 
-      // set from
-      escrow.from = ctx.accounts.from.key();
-
-      // set to
-      escrow.to = ctx.accounts.to.key();
-
-      // set amount
-      escrow.amount = amount;
-
-      Ok(())
+        Ok(())
     }
+
+//     pub fn withdraw_from_escrow(ctx: Context<WithdrawFromEscrow>) -> Result<()> {
+//         let escrow = &ctx.accounts.escrow;
+
+//         // Ensure the withdrawer is the initializer
+//         require!(
+//             escrow.initializer == ctx.accounts.withdrawer.key(),
+//             EscrowError::UnauthorizedWithdrawal
+//         );
+
+//         // Transfer tokens from escrow account back to initializer
+//         token::transfer(
+//             CpiContext::new_with_signer(
+//                 ctx.accounts.token_program.to_account_info(),
+//                 Transfer {
+//                     from: ctx.accounts.escrow_token_account.to_account_info(),
+//                     to: ctx.accounts.withdrawer_token_account.to_account_info(),
+//                     authority: ctx.accounts.escrow_token_account.to_account_info(),
+//                 },
+//                 &[&[
+//                     b"escrow",
+//                     ctx.accounts.escrow.to_account_info().key.as_ref(),
+//                     &[*ctx.bumps.get("escrow_token_account").unwrap()],
+//                 ]]
+//             ),
+//             escrow.amount,
+//         )?;
+
+//         // Close the escrow account
+//         ctx.accounts.escrow.close(ctx.accounts.withdrawer.to_account_info())?;
+
+//         Ok(())
+//     }
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize)]
+pub struct InitializeEscrowParams {
+    pub amount: u64,
 }
 
 #[derive(Accounts)]
-pub struct CreateEscrow<'info> {
-  // escrow account PDA
-  #[account(
-    init,
-    seeds = [b"escrow".as_ref(), from.key().as_ref(), to.key().as_ref()],
-    bump,
-    payer = from,
-    space = size_of::<EscrowAccount>()
-  )]
+#[instruction(params: InitializeEscrowParams)]
+pub struct InitializeEscrow<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  pub escrow: Account<'info, EscrowAccount>,
+    #[account(
+      mut,
+      associated_token::mint = mint,
+      associated_token::authority = owner,
+      constraint = owner_token_account.amount >= params.amount,
+    )]
+    pub owner_token_account: Account<'info, TokenAccount>,
 
-  #[account(mut)]
-  pub from: Signer<'info>,
-  /// CHECK: safe
-  // lol bypass safe check
-  #[account(mut)]
-  pub to: AccountInfo<'info>,
+    #[account(
+        init_if_needed,
+        payer = owner,
+        associated_token::mint = mint,
+        associated_token::authority = escrow,
+    )]
+    pub escrow_token_account: Account<'info, TokenAccount>,
 
-  pub system_program: Program<'info, System>
+    #[account(
+      init_if_needed,
+      payer = owner,
+      seeds=[b"escrow", owner.key().as_ref()],
+      bump,
+      space = 8 + 8 + 32
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
+// #[derive(Accounts)]
+// pub struct WithdrawFromEscrow<'info> {
+//     #[account(mut)]
+//     pub withdrawer: Signer<'info>,
+//     #[account(mut)]
+//     pub withdrawer_token_account: Account<'info, TokenAccount>,
+//     #[account(
+//         mut,
+//         seeds = [b"escrow", escrow.key().as_ref()],
+//         bump,
+//         token::mint = withdrawer_token_account.mint,
+//         token::authority = escrow_token_account,
+//     )]
+//     pub escrow_token_account: Account<'info, TokenAccount>,
+//     #[account(mut, has_one = owner)]
+//     pub escrow: Account<'info, Escrow>,
+//     pub token_program: Program<'info, Token>,
+// }
+
 #[account]
-pub struct EscrowAccount {
-  pub from: Pubkey,
-  pub to: Pubkey,
-  pub amount: u64
+pub struct Escrow {
+    pub bump: u8,
+    pub owner: Pubkey,
+}
+
+#[error_code]
+pub enum EscrowError {
+    #[msg("Only the escrow owner can withdraw funds")]
+    UnauthorizedWithdrawal,
 }
--- tests/test-app-pda.ts
+++ tests/test-app-pda.ts
@@ -9,30 +9,12 @@ describe("test-app-pda", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.TestAppPda as Program<TestAppPda>;
+  before(async () => {
+
+  })
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const publicKey = anchor.AnchorProvider.local().wallet.publicKey;
-    const toWallet: anchor.web3.Keypair = anchor.web3.Keypair.generate();
-    const [escrowPDA] = await anchor.web3.PublicKey.findProgramAddressSync([
-        utf8.encode('escrow'),
-        publicKey.toBuffer(),
-        toWallet.publicKey.toBuffer()
-      ],
-      program.programId
-    );
-    console.log("escrowPDA", escrowPDA);
-    const initAccounts = {
-      from: publicKey,
-      to: toWallet.publicKey,
-      systemProgram:  anchor.web3.SystemProgram.programId,
-      escrow: escrowPDA
-    };
-    await program.methods.createEscrow(new BN(32)).accounts(initAccounts).rpc();
-    const escrowAccount = await program.account.escrowAccount.fetch(escrowPDA);
-    console.log(escrowAccount);
-    assert.equal(escrowAccount.amount.toNumber(), 32);
-    assert.isTrue(escrowAccount.from.equals(publicKey));
-    assert.isTrue(escrowAccount.to.equals(toWallet.publicKey));
+
   });
 });
