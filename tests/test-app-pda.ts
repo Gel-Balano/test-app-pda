91dc4a7d9240d18c1e777e1c7b6c9ff165168775
Gel <angel.balano@gmail.com>
2024-08-10 13:30:48 +0800

    Update test

--- tests/test-app-pda.ts
+++ tests/test-app-pda.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { TestAppPda } from "../target/types/test_app_pda";
+import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import { assert } from "chai";
 
 describe("test-app-pda", () => {
   // Configure the client to use the local cluster.
@@ -10,7 +12,27 @@ describe("test-app-pda", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const publicKey = anchor.AnchorProvider.local().wallet.publicKey;
+    const toWallet: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const [escrowPDA] = await anchor.web3.PublicKey.findProgramAddressSync([
+        utf8.encode('escrow'),
+        publicKey.toBuffer(),
+        toWallet.publicKey.toBuffer()
+      ],
+      program.programId
+    );
+    console.log("escrowPDA", escrowPDA);
+    const initAccounts = {
+      from: publicKey,
+      to: toWallet.publicKey,
+      systemProgram:  anchor.web3.SystemProgram.programId,
+      escrow: escrowPDA
+    };
+    await program.methods.createEscrow(new BN(32)).accounts(initAccounts).rpc();
+    const escrowAccount = await program.account.escrowAccount.fetch(escrowPDA);
+    console.log(escrowAccount);
+    assert.equal(escrowAccount.amount.toNumber(), 32);
+    assert.isTrue(escrowAccount.from.equals(publicKey));
+    assert.isTrue(escrowAccount.to.equals(toWallet.publicKey));
   });
 });
